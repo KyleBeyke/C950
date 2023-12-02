0817416bbe5750cd0d6a922e7f64d0a3eaa09249
Kyle Beyke <62480503+KyleBeyke@users.noreply.github.com>
2023-12-02 06:28:20 -0600

    Refactor Package and WGUPS classes for better code organization
    Reorganized the Package and WGUPS classes to improve code structure and
    readability. Initialized Package attributes directly with provided
    values and defined an iterator for object attributes. Added comments to
    clarify the purpose of certain attributes. Updated load_packages and
    load_distance_data methods to handle CSV file parsing and data loading.
    Introduced error handling for float conversion and collision handling
    for hash table insertion.
    
    No issue references.

--- deliveries_v2.py
+++ deliveries_v2.py
@@ -4,6 +4,7 @@ from datetime import datetime, timedelta
 
 class Package:
     def __init__(self, package_id, address, city, state, zip_code, deadline, weight, notes=None):
+        # Initialize Package attributes with provided values
         self.package_id = package_id
         self.address = address
         self.city = city
@@ -12,10 +13,11 @@ class Package:
         self.deadline = deadline
         self.weight = weight
         self.notes = notes
-        self.delivery_status = "At Hub"
-        self.delivery_time = None
-    
+        self.location = "Hub"  # Initial location is set to "Hub"
+        self.delivery_time = None  # Initialize delivery time as None
+
     def __iter__(self):
+        # Define an iterator to yield key-value pairs for object attributes
         yield 'package_id', self.package_id
         yield 'address', self.address
         yield 'city', self.city
@@ -24,69 +26,91 @@ class Package:
         yield 'deadline', self.deadline
         yield 'weight', self.weight
         yield 'notes', self.notes
-        yield 'delivery_status', self.delivery_status
+        yield 'location', self.location
         yield 'delivery_time', self.delivery_time
+
 
 class Truck:
     def __init__(self, truck_id, max_capacity=16):
+        # Initialize Truck attributes with provided values
         self.truck_id = truck_id
         self.max_capacity = max_capacity
-        self.packages = []
+        self.packages = []  # List to store packages loaded onto the truck
+
 
 class WGUPS:
     def __init__(self):
-        self.trucks = [Truck(1), Truck(2), Truck(3)]  # Three trucks
-        self.distance_table = {}
-        self.hash_table = {}
+        # Initialize the WGUPS (World-Wide Package Unloading System)
+        self.trucks = [Truck(1), Truck(2), Truck(3)]  # Three trucks are created
+        self.distance_table = {}  # Dictionary to store distances between hubs
+        self.hash_table = {}  # Dictionary to store packages hashed by package_id
+
 
     def load_packages(self, file_path):
-            with open(file_path, mode='r') as file:
-                flag = False
-                csv_reader = csv.reader(file)
-                for row in csv_reader:
-                    if row[0] == "Package\nID":
-                        flag = True
-                        print('true')
-                    if (row[0] != "Package\nID" and flag): 
-                        package = Package(*row[:7])
-                        self.insert_into_hash_table(package)
+        # Open the CSV file in read mode
+        with open(file_path, mode='r') as file:
+            # Initialize a flag to identify the start of package data
+            flag = False
+            # Create a CSV reader object
+            csv_reader = csv.reader(file)
+            
+            # Iterate through each row in the CSV file
+            for row in csv_reader:
+                # Check if the current row indicates the start of package data
+                if row[0] == "Package\nID":
+                    # Set the flag to True when the start is found
+                    flag = True
+                    print('true')
+                
+                # Check if the flag is True and the current row is not a header
+                if (row[0] != "Package\nID" and flag):
+                    # Create a Package object with the data from the row
+                    package = Package(*row[:7])
+                    
+                    # Print the attributes of the Package object
+                    print(*package)
+                    
+                    # Insert the Package object into the hash table
+                    self.insert_into_hash_table(package)
 
-    def load_distance_data(self, filepath = 'distance_table.csv'):
+
+    def load_distance_data(self, filepath='distance_table.csv'):
+        # List to store source values
         sources = []
+
         # Read CSV file and populate the hash table
         with open(filepath, 'r') as file:
             flag = False
             reader = csv.reader(file, dialect='excel')
+            
+            # Iterate through each row in the CSV file
             for row in reader:
+                # Check if the current row is the header row
                 if row[0] == 'DISTANCE BETWEEN HUBS IN MILES':
                     flag = True
+                    
+                    # Iterate through sources in the row (from index 2 to 30)
                     for source in row[2:31]:
-                        # Find the index of the newline character
-                        newline_index = source.find('\n')
-                        # Disregard content before the newline character
-                        substring_after_newline = source[newline_index + 1:]
-                        # Keep the characters until the comma
-                        address_before_comma = substring_after_newline.split(',')[0]
-                        # Strip any leading or trailing whitespace
-                        source = address_before_comma.strip()
-                        sources.append(source)
+                        # Split the string at newline characters, take the second part
+                        # Split the result at commas, take the first part
+                        # Strip any leading or trailing whitespace and append to the list
+                        sources.append(source.split('\n')[1].split(',')[0].strip())
                 elif flag:
-                    destination = row[0]
-                    # Find the index of the newline character
-                    newline_index = destination.find('\n')
-                    # Disregard content before the newline character
-                    substring_after_newline = destination[newline_index + 1:]
-                    # Keep the characters until the comma
-                    address_before_comma = substring_after_newline.split(',')[0]
-                    # Strip any trailing or leading whitespace
-                    destination = address_before_comma.strip()
+                    # Split the string at newline characters, take the second part
+                    # Split the result at commas, take the first part
+                    # Strip any leading or trailing whitespace and assign to 'destination'
+                    destination = row[0].split('\n')[1].split(',')[0].strip()
                     index = 0
+                    
+                    # Iterate through each source
                     for source in sources:
                         try:
                             distance = row[2 + index]
                             float_value = float(distance)
+                            
+                            # Check if the distance is greater than 0
                             if float_value > 0:
-                                # Check if the source exists
+                                # Check if the source exists in the distance_table
                                 if source in self.distance_table:
                                     # If source exists, append to the existing dictionary
                                     self.distance_table[source][destination] = float_value
@@ -94,24 +118,43 @@ class WGUPS:
                                     # If source doesn't exist, add a new entry for the source
                                     self.distance_table[source] = {destination: float_value}
                             else:
-                              pass
+                                # Handle case where distance is not greater than 0
+                                pass
                         except ValueError:
+                            # Handle case where conversion to float fails
                             pass
                         index += 1
                 else:
+                    # Skip rows until the header row is found
                     pass
 
-    def insert_into_hash_table(self, package):
-        index = hash(package.package_id) % 100
-        if index not in self.hash_table:
-            self.hash_table[index] = package
 
-    def look_up_package(self, package_id):
-        index = hash(package_id) % 100
-        if index in self.hash_table:
-            package = self.hash_table[index]
-            return package
-        return None
+    def insert_into_hash_table(self, package):
+        # Use package_id as the key for hashing
+        key = package.package_id
+        index = hash(key) % 100
+        
+        # Check if the index exists in the hash_table
+        if index not in self.hash_table:
+            self.hash_table[index] = {key: package}
+        else:
+            # If the index exists, check if the key exists
+            if key in self.hash_table[index]:
+                # Handle collision by chaining (using a list to store multiple values at the same index)
+                self.hash_table[index][key].append(package)
+            else:
+                self.hash_table[index][key] = package
+
+    def look_up_package(self, address):
+
+        for index in self.hash_table:
+            for package_id, package in self.hash_table[index].items():
+                # Check if the address matches
+                if package.address == address:
+                    ([]).append(package)
+
+        # Return the list of matching packages
+        return []
 
 def main():
     # Create an instance of WGUPS
@@ -124,31 +167,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-#             source = row['Source']
-#             destination = row['Destination']
-#             distance = float(row['Distance'])  # Convert distance to a numerical value if needed
-
-#             # If source is not in the graph, add it
-#             if source not in graph:
-#                 graph[source] = {}
-
-#             # Add the destination and distance to the source vertex
-#             graph[source][destination] = distance
-
-#             # If the graph is undirected, add the reverse edge
-#             if destination not in graph:
-#                 graph[destination] = {}
-#             graph[destination][source] = distance
-
-#     return graph
-
-# # Example CSV file (replace 'your_file.csv' with the actual file path)
-# file_path = 'your_file.csv'
-
-# # Load distance data into a hash table (dictionary)
-# distance_hash_table = load_distance_data(file_path)
-
-# # Print the resulting hash table
-# print(distance_hash_table)
